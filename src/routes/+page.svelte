339bffcd1d92672e6bf08f8cbe5784a5d293a90c
as43z <albert.saez.nunez@estudiantat.upc.edu>
2022-11-10 22:54:22 +0100

    links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,15 +13,15 @@
 </div>
 <div id="escaparate" class="mt-10 md:mt-20 md:flex-grow self-center font-lato grid gap-6 ss:grid-cols-1 ss:justify-items-center md:grid-flow-col md:auto-cols-max ss:text-sm md:text-xl">
     <div id="repos" class="">
-        <a id="btn-repos" class="cursor-pointer bg-transparent hover:bg-title hover:text-white py-2 px-2 border border-title rounded">Repos</a>
+        <a href="https://github.com/as43z" id="btn-repos" class="cursor-pointer bg-transparent hover:bg-title hover:text-white py-2 px-2 border border-title rounded">Repos</a>
     </div>
     <div id="projects" class="">
-        <a id="btn-projects" class="cursor-pointer bg-transparent hover:bg-title hover:text-white py-2 px-2 border border-title rounded">Projects & Contests</a>
+        <a id="btn-projects" data-tooltip-target="tooltip-default" class="cursor-not-allowed bg-gray-400 py-2 px-2 border border-gray-400  rounded">Projects & Contests (Soon!)</a>
     </div>
     <div id="articles" class="">
-        <a id="btn-articles" class="cursor-pointer bg-transparent hover:bg-title hover:text-white py-2 px-2 border border-title rounded">Articles & Papers</a>
+        <a href="https://arxiv.org/search/?query=A.+Saez+Nu%C3%B1ez&searchtype=author&abstracts=show" id="btn-articles" class="cursor-pointer bg-transparent hover:bg-title hover:text-white py-2 px-2 border border-title rounded">Articles & Papers</a>
     </div>
 </div>
-<div id="contact" class="flex flex-wrap ss:justify-items-left ss:ml-10 md:justify-center mt-20 flex-stretch font-lato">
+<div id="contact" class="flex flex-wrap ss:justify-items-left md:justify-center flex-stretch font-lato">
     <h2 class="md:text-center text-subtitle md:text-3xl ss:text-sm ml-10 mr-10">And if you want to get in contact with me: <a href="mailto:albertisgmaeng@gmail.com" class="text-blue-600">send a mail!</a> I'll read it and get in touch ASAP.</h2>
 </div>
